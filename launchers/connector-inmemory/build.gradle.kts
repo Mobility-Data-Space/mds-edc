e92dd046bca2fc83c6e8e027cf10ebfbfda9734e
ndr_brt <andrea.bertagnolli@gmail.com>
2025-07-03 09:58:08 +0200

    feat: emit retirement events

--- launchers/connector-inmemory/build.gradle.kts
+++ launchers/connector-inmemory/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 
     runtimeOnly(libs.edc.iam.mock)
 
+    implementation(project(":extensions:contract-retirement"))
     implementation(project(":extensions:manual-negotiation-approval"))
     implementation(project(":extensions:policy:policy-always-true"))
     implementation(project(":extensions:policy:policy-referring-connector"))
