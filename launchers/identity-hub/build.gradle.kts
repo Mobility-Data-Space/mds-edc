50e19e2df0e09a5c0fb89f2bd7d260f404b269e3
ndr_brt <andrea.bertagnolli@gmail.com>
2025-11-05 14:51:51 +0100

    identityhub postgresql persistence

--- launchers/identity-hub/build.gradle.kts
+++ launchers/identity-hub/build.gradle.kts
@@ -9,6 +9,9 @@ val edcGroupId = "org.eclipse.edc"
 
 dependencies {
     runtimeOnly(libs.edc.identityhub.bom)
+    runtimeOnly(libs.edc.identityhub.feature.sql.bom)
+
+    implementation(project(":extensions:identity-hub:database-schema-migration-identity-hub"))
     implementation(project(":extensions:identity-hub:super-user-seeder"))
 }
 
