a8a6256a91424a650dae01e46b38e58c7e797610
andrea bertagnolli <andrea.bertagnolli@gmail.com>
2026-03-23 14:00:16 +0100

    feat: stabilize ACLs cleanup by getting the admin properties directly from the DataFlow (#392)

--- extensions/kafka/data-plane-kafka/src/main/java/eu/dataspace/connector/extension/kafka/broker/acls/KafkaAccessControlLists.java
+++ extensions/kafka/data-plane-kafka/src/main/java/eu/dataspace/connector/extension/kafka/broker/acls/KafkaAccessControlLists.java
@@ -3,6 +3,7 @@ package eu.dataspace.connector.extension.kafka.broker.acls;
 import eu.dataspace.connector.dataplane.kafka.spi.AccessControlLists;
 import eu.dataspace.connector.dataplane.kafka.spi.AllowResponse;
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.acl.AccessControlEntry;
 import org.apache.kafka.common.acl.AccessControlEntryFilter;
 import org.apache.kafka.common.acl.AclBinding;
@@ -13,18 +14,18 @@ import org.apache.kafka.common.resource.PatternType;
 import org.apache.kafka.common.resource.ResourcePattern;
 import org.apache.kafka.common.resource.ResourcePatternFilter;
 import org.apache.kafka.common.resource.ResourceType;
+import org.eclipse.edc.connector.dataplane.spi.store.DataPlaneStore;
 import org.eclipse.edc.spi.result.ServiceResult;
 import org.eclipse.edc.spi.security.Vault;
 import org.eclipse.edc.spi.types.domain.DataAddress;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
 
 import static eu.dataspace.connector.dataplane.kafka.spi.KafkaBrokerDataAddressSchema.KAFKA_ADMIN_PROPERTIES_KEY;
 import static eu.dataspace.connector.dataplane.kafka.spi.KafkaBrokerDataAddressSchema.TOPIC;
@@ -33,28 +34,21 @@ public class KafkaAccessControlLists implements AccessControlLists {
 
     public static final String KAFKA_PRINCIPAL_NAME_KEY_PREFIX = "kafka-principal-name-";
     private final Vault vault;
-    // TODO: please note that this needs to change, likely upstream (likely revoke method should get the data address)
-    private final ConcurrentHashMap<String, Properties> adminPropertiesCache = new ConcurrentHashMap<>();
+    private final DataPlaneStore dataPlaneStore;
 
-    public KafkaAccessControlLists(Vault vault) {
+    public KafkaAccessControlLists(Vault vault, DataPlaneStore dataPlaneStore) {
         this.vault = vault;
+        this.dataPlaneStore = dataPlaneStore;
     }
 
     @Override
     public ServiceResult<AllowResponse> allowAccessTo(String principalName, String dataFlowId, DataAddress dataAddress) {
-        var adminPropertiesKey = dataAddress.getStringProperty(KAFKA_ADMIN_PROPERTIES_KEY);
-        var adminProperties = vault.resolveSecret(adminPropertiesKey);
         var groupId = UUID.randomUUID().toString();
 
-        return adminProperties(adminProperties)
-                .compose(properties -> {
-                    adminPropertiesCache.put(dataFlowId, properties);
-                    return createAcls(properties,
-                            userCanAccess(principalName, ResourceType.TOPIC, dataAddress.getStringProperty(TOPIC)),
-                            userCanAccess(principalName, ResourceType.GROUP, groupId)
-                    );
-                })
-                .onSuccess(a -> vault.storeSecret(KAFKA_PRINCIPAL_NAME_KEY_PREFIX + dataFlowId, principalName))
+        return onKafkaAdminDo(dataAddress, createAcls(
+                userCanAccess(principalName, ResourceType.TOPIC, dataAddress.getStringProperty(TOPIC)),
+                userCanAccess(principalName, ResourceType.GROUP, groupId)))
+                .onSuccess(i -> vault.storeSecret(KAFKA_PRINCIPAL_NAME_KEY_PREFIX + dataFlowId, principalName))
                 .map(v -> new AllowResponse(groupId));
     }
 
@@ -65,30 +59,46 @@ public class KafkaAccessControlLists implements AccessControlLists {
             return ServiceResult.notFound("Principal name not found for data flow " + dataFlowId);
         }
 
-        var properties = adminPropertiesCache.get(dataFlowId);
-
-        try (var adminClient = AdminClient.create(properties)) {
-            adminClient.deleteAcls(List.of(new AclBindingFilter(
-                    ResourcePatternFilter.ANY,
-                    new AccessControlEntryFilter("User:" + principalName, "*", AclOperation.READ, AclPermissionType.ALLOW)
-            ))).all().get();
-        } catch (ExecutionException | InterruptedException e) {
-            return ServiceResult.unexpected("Cannot delete ACLs: " + e.getMessage());
+        var dataFlow = dataPlaneStore.findById(dataFlowId);
+        if (dataFlow == null) {
+            return ServiceResult.notFound("Cannot retrieve DataFlow %s to deny access".formatted(dataFlowId));
         }
 
-        vault.deleteSecret(KAFKA_PRINCIPAL_NAME_KEY_PREFIX + dataFlowId);
-
-        return ServiceResult.success();
+        return onKafkaAdminDo(dataFlow.getActualSource(), deleteAcls(anyAllowReadPermission(principalName)))
+                .onSuccess(i -> vault.deleteSecret(KAFKA_PRINCIPAL_NAME_KEY_PREFIX + dataFlowId));
     }
 
-    private ServiceResult<Void> createAcls(Properties adminProperties, AclBinding... bindings) {
-        try (var adminClient = AdminClient.create(adminProperties)) {
-            adminClient.createAcls(Arrays.stream(bindings).toList()).all().get();
-        } catch (ExecutionException | InterruptedException e) {
-            return ServiceResult.unexpected("Cannot create ACLs: " + e.getMessage());
+    private ServiceResult<Void> onKafkaAdminDo(DataAddress dataAddress, Function<AdminClient, KafkaFuture<?>> action) {
+        var adminPropertiesKey = dataAddress.getStringProperty(KAFKA_ADMIN_PROPERTIES_KEY);
+        if (adminPropertiesKey == null) {
+            return ServiceResult.unexpected("Source DataAddress doesn't contain mandatory key " + KAFKA_ADMIN_PROPERTIES_KEY);
+        }
+        var plainProperties = vault.resolveSecret(adminPropertiesKey);
+        if (plainProperties == null) {
+            return ServiceResult.unexpected("Cannot get Kafka Admin properties from Vault key " + adminPropertiesKey);
         }
 
-        return ServiceResult.success();
+        var adminProperties = new Properties();
+        try (var reader = new StringReader(plainProperties)) {
+            adminProperties.load(reader);
+        } catch (IOException e) {
+            return ServiceResult.unexpected("Cannot parse Kafka Admin properties: " + e.getMessage());
+        }
+
+        try (var adminClient = AdminClient.create(adminProperties)) {
+            action.apply(adminClient).get();
+            return ServiceResult.success();
+        } catch (Exception e) {
+            return ServiceResult.unexpected("Cannot create ACLs: " + e.getMessage());
+        }
+    }
+
+    private Function<AdminClient, KafkaFuture<?>> createAcls(AclBinding... bindings) {
+        return adminClient -> adminClient.createAcls(Arrays.stream(bindings).toList()).all();
+    }
+
+    private Function<AdminClient, KafkaFuture<?>> deleteAcls(AclBindingFilter... bindingFilters) {
+        return adminClient -> adminClient.deleteAcls(Arrays.stream(bindingFilters).toList()).all();
     }
 
     private AclBinding userCanAccess(String principalName, ResourceType resourceType, String resourceName) {
@@ -97,13 +107,11 @@ public class KafkaAccessControlLists implements AccessControlLists {
         return new AclBinding(pattern, entry);
     }
 
-    private ServiceResult<Properties> adminProperties(String serializedProperties) {
-        var properties = new Properties();
-        try (var reader = new StringReader(serializedProperties)) {
-            properties.load(reader);
-        } catch (IOException e) {
-            return ServiceResult.unexpected("Cannot get Kafka Admin properties: " + e.getMessage());
-        }
-        return ServiceResult.success(properties);
+    private @NotNull AclBindingFilter anyAllowReadPermission(String principalName) {
+        return new AclBindingFilter(
+                ResourcePatternFilter.ANY,
+                new AccessControlEntryFilter("User:" + principalName, "*", AclOperation.READ, AclPermissionType.ALLOW)
+        );
     }
+
 }
