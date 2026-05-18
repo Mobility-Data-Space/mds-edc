5656c44de8c30fcb7dcabc56d1bce1ada9dab975
Ramy Hcini <ramyhcini@think-it.io>
2026-05-18 11:28:30 +0200

    ci: add DCP runtimes to Trivy image scan matrix

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -28,7 +28,6 @@ dependencies {
     testImplementation(libs.testcontainers.kafka)
     testImplementation(libs.testcontainers.postgresql)
     testImplementation(libs.testcontainers.vault)
-    testFixturesApi(libs.testcontainers.junit.jupiter)
 
     testRuntimeOnly(libs.dsp.tck.metadata)
     testRuntimeOnly(libs.dsp.tck.catalog)
