69b8d6d527ae850e891feb43fcc4bd9244b6f2c3
Mayssen Aidoudi <mayssenaidoudi@think-it.io>
2026-06-18 14:04:04 +0100

    Revert "test: add migration tests for wallet identity-hub schema"
    This reverts commit 001f47bb47b317c76c80b528d08585c936a2f56f.

--- extensions/identity-hub/database-schema-migration-wallet/build.gradle.kts
+++ extensions/identity-hub/database-schema-migration-wallet/build.gradle.kts
@@ -10,13 +10,4 @@ dependencies {
 
     implementation(libs.flyway.core)
     runtimeOnly(libs.flyway.database.postgres)
-
-    testImplementation(libs.edc.junit)
-    testImplementation(libs.assertj)
-    testImplementation(libs.junit.jupiter)
-    testImplementation(libs.junit.platform.launcher)
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.postgres)
-    testImplementation(libs.testcontainers.junit.jupiter)
-    testImplementation(libs.testcontainers.postgresql)
 }
