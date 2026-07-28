ab59bdc083f57c271b6ce8212060a0b5e265dceb
Ramy Hcini <ramyhcini@think-it.io>
2026-07-28 14:47:36 +0200

    refactor: remove edp extension and launcher

--- extensions/edp/src/main/java/io/nexyo/edp/extensions/dtos/internal/DaseenCreateEntryRequestDto.java
+++
@@ -1,7 +0,0 @@
-package io.nexyo.edp.extensions.dtos.internal;
-
-/**
- * DTO for the request of a daseen entry.
- */
-public record DaseenCreateEntryRequestDto(String contractId) {
-}
--- extensions/edp/src/main/java/io/nexyo/edp/extensions/dtos/internal/EdpsResultRequestDto.java
+++
@@ -1,7 +0,0 @@
-package io.nexyo.edp.extensions.dtos.internal;
-
-/**
- * DTO for the request of a result.
- */
-public record EdpsResultRequestDto(String destinationAddress) {
-}
