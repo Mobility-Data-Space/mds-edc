412030ac68eb6b337ee5d68d3a1e5a556cd2173a
ndr_brt <andrea.bertagnolli@gmail.com>
2026-09-08 10:35:20 +0200

    map asset dataAddress to dataplaneMetadata.properties

--- extensions/data-address-store-patch/build.gradle.kts
+++ extensions/data-address-store-patch/build.gradle.kts
@@ -8,8 +8,12 @@ dependencies {
     api(libs.edc.json.ld.spi)
     api(libs.edc.transfer.spi)
     api(libs.edc.transform.spi)
+    api(libs.edc.web.spi)
 
+    implementation(libs.edc.asset.api)
+    implementation(libs.edc.jersey.providers.lib)
     implementation(libs.edc.control.plane.transfer)
+    implementation(libs.jakarta.annotation.api)
 
     testImplementation(libs.edc.junit)
 }
